2f098e7a7e7e037952416b33dd005fa549161272
Morteza Taghaddomi <mortezataqaddomi99@gmail.com>
2020-04-20 13:35:07 +0430

    create dependencies

--- src/main/java/ir/ac/kntu/Main.java
+++ src/main/java/ir/ac/kntu/Main.java
@@ -1,5 +1,12 @@
 package ir.ac.kntu;
 
+import ir.ac.kntu.logic.ConsoleGraphicsEngine;
+import ir.ac.kntu.logic.Director;
+import ir.ac.kntu.logic.GraphicsEngine;
+
 public class Main {
+    GraphicsEngine engine = new ConsoleGraphicsEngine(); 
+    Director director = new Director(graphicsEngine); 
+    
     //TODO: Start your game from here
 }
